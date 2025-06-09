d1ce563b409cf39110336cc22fb0671c16d8aa0d
caonguyen-tran <tcnguyen.prof@gmail.com>
2025-06-09 21:29:05 +0700

    config change password for user module

--- project/app/users/serializer.py
+++ project/app/users/serializer.py
@@ -1,12 +1,15 @@
+from dataclasses import fields
+
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
 from .models import User
+
+user_fields = ('id', 'email', 'password', 'full_name', 'avatar', 'date_joined', 'is_active', 'is_staff', 'is_superuser', 'last_login')
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
-        # Thêm thông tin vào payload nếu muốn
         token['email'] = user.email
         token['username'] = user.username
         return token
@@ -14,15 +17,31 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True)
     password = serializers.CharField(write_only=True, min_length=6)
+    full_name = serializers.CharField(required=True, min_length=6)
 
     class Meta:
         model = User
-        fields = ('id', 'email', 'password', 'full_name')
+        fields = user_fields
 
     def validate_email(self, value):
         if User.objects.filter(email=value).exists():
-            raise serializers.ValidationError("Already exited Email !")
+            raise serializers.ValidationError("Email already exists!")
         return value
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+
+    def validate(self, attrs):
+        user = self.context['request'].user
+
+        if not user.check_password(attrs['old_password']):
+            raise serializers.ValidationError("Incorrect old password!")
+
+        if user.check_password(attrs['new_password']):
+            raise serializers.ValidationError("New password must not be same as old password!")
+
+        return attrs
--- project/app/users/views.py
+++ project/app/users/views.py
@@ -1,12 +1,29 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializer import CustomTokenObtainPairSerializer, UserSerializer
-from rest_framework import viewsets,mixins
+from .serializer import CustomTokenObtainPairSerializer, UserSerializer, ChangePasswordSerializer
+from rest_framework import viewsets, mixins, generics, status
 from .models import User
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def get_serializer_class(self):
+        if self.action == 'update_password':
+            return ChangePasswordSerializer
+        return UserSerializer
+
+    @action(methods=['patch'], detail=False, url_path='update-password', url_name='update-password')
+    def update_password(self, request):
+        serializer = self.get_serializer(data=request.data, context={'request': request})
+
+        if serializer.is_valid():
+            user = request.user
+            user.set_password(serializer.validated_data['password'])
+            user.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
